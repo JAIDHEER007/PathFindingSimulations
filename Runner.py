3f8043e03d17905fcd4800b6416f7a5ce78363a0
jaidheer7 <sirigineedi.jaidheer@go-yubi.com>
2023-02-28 11:51:02 +0530

    Modifications to Runner

--- Runner.py
+++ Runner.py
@@ -1,15 +1,18 @@
 from Algorithms.golGenerator import gol
 import numpy as np
+import sys
+import os
 
 if __name__ == '__main__':
     # Set the current working dir to grids > gol
-    cwd = 'grids\gol'
+    cwd = sys.path[0]
+    fPath = os.path.join(cwd, 'grids', 'gol')
 
-    rows = 75
-    cols = 75
+    rows = 200
+    cols = 200
     
     grid = np.random.choice(a = [True, False], size = (rows, cols))
     
-    golObj = gol(startGrid = grid, generations = 30, cwd = cwd)
+    golObj = gol(startGrid = grid, generations = 30, cwd = fPath)
 
     golObj.start()
