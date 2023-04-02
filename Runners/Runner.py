84b94d11e6aebe59a3594790082b0f2895eb87fd
jaidheer7 <sirigineedi.jaidheer@go-yubi.com>
2023-04-02 21:32:07 +0530

    MInor Changes

--- Runners/Runner.py
+++ Runners/Runner.py
@@ -70,10 +70,10 @@ assert endingLocation == bfsObject.start()
 print("Found the ending Location")
 
 
-# for zoomFactor in [3]:
-#   VideoGenerator_v2.saveVideo(fPath = fPath, fps = 250, stateMatrix = grid.stateMatrix(), 
-#                               zoomFactor = zoomFactor, videoName = f"Output_z{zoomFactor}")
-#   print("Video Generated")
+for zoomFactor in [3]:
+  VideoGenerator_v2.saveVideo(fPath = fPath, fps = 250, stateMatrix = grid.stateMatrix(), 
+                              zoomFactor = zoomFactor, videoName = f"Output_z{zoomFactor}")
+  print("Video Generated")
 
 
 
